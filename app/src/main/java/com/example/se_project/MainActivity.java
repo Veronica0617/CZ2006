6aa1623d3260bce635bbd3db00dfb6a821d165c5
Veronica <u3532975@connect.hku.hk>
2018-11-05 14:46:38 +0800

    add a sort class

--- app/src/main/java/com/example/se_project/MainActivity.java
+++ app/src/main/java/com/example/se_project/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //hi
         trial = (TextView)findViewById(R.id.trial);
-        new GetPM25().execute();
+           new GetPM25().execute();
     }
+
 }
