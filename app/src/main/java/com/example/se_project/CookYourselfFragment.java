86fd81e4c88130defffa9623e1bb21493e102a76
mavishung <tmmhung@connect.ust.hk>
2018-11-09 18:04:48 +0800

    map gym & caterer

---
+++ app/src/main/java/com/example/se_project/CookYourselfFragment.java
@@ -0,0 +1,30 @@
+package com.example.se_project;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CookYourselfFragment extends Fragment {
+
+
+    public CookYourselfFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View RootView = inflater.inflate(R.layout.fragment_cook_yourself, container, false);
+        return RootView;
+    }
+
+}
--- app/src/main/java/com/example/se_project/Healthy_DietFragment.java
+++ app/src/main/java/com/example/se_project/Healthy_DietFragment.java
@@ -4,14 +4,20 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 
 public class Healthy_DietFragment extends Fragment {
-
+    FrameLayout main_frame;
+    ImageButton cook;
+    ImageButton out;
+    HealthierCaterersMap healthierCaterersMap;
+    CookYourselfFragment cookYourselfFragment;
     public Healthy_DietFragment() {
         // Required empty public constructor
     }
@@ -22,8 +28,31 @@ public class Healthy_DietFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_healthy__diet, container, false);
+        View RootView = inflater.inflate(R.layout.fragment_healthy__diet, container, false);
+        main_frame = (FrameLayout)RootView.findViewById(R.id.main_frame);
+        healthierCaterersMap = new HealthierCaterersMap();
+        cookYourselfFragment = new CookYourselfFragment();
+        cook = (ImageButton)RootView.findViewById(R.id.cook_yourself);
+        out = (ImageButton)RootView.findViewById(R.id.dining_out);
+        cook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setFragment(cookYourselfFragment);
+            }
+        });
+        out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setFragment(healthierCaterersMap);
+            }
+        });
+        return RootView;
     }
 
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
+    }
 
 }
