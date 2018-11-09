9dd20a89ea8466701127e7553f1db461e7e55bfb
mavishung <tmmhung@connect.ust.hk>
2018-11-09 17:55:56 +0800

    map gym & caterer

--- app/src/main/java/com/example/se_project/SearchFragment.java
+++ app/src/main/java/com/example/se_project/SearchFragment.java
@@ -4,14 +4,20 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 
 public class SearchFragment extends Fragment {
-
+    FrameLayout main_frame;
+    ImageButton pharmacy;
+    ImageButton medicine;
+    SearchPharmaciesFragment searchPharmaciesFragment;
+    SearchMedicineFragment searchMedicineFragment;
     public SearchFragment() {
         // Required empty public constructor
     }
@@ -20,8 +26,30 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View RootView = inflater.inflate(R.layout.fragment_search, container, false);
+        main_frame = (FrameLayout)RootView.findViewById(R.id.main_frame);
+        pharmacy = (ImageButton)RootView.findViewById(R.id.search_pharmacies);
+        medicine = (ImageButton)RootView.findViewById(R.id.search_medicine);
+        searchPharmaciesFragment = new SearchPharmaciesFragment();
+        searchMedicineFragment = new SearchMedicineFragment();
+        pharmacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setFragment(searchPharmaciesFragment);
+            }
+        });
+        medicine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setFragment(searchMedicineFragment);
+            }
+        });
+        return RootView;
     }
-
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
+    }
 
 }
