2d5e079de89921eed57dbd6fedbdadf34f81396a
mavishung <tmmhung@connect.ust.hk>
2018-11-09 17:44:53 +0800

    map gym & caterer

--- app/src/main/java/com/example/se_project/ExerciseFragment.java
+++ app/src/main/java/com/example/se_project/ExerciseFragment.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 
 /**
@@ -18,6 +21,11 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ExerciseFragment extends Fragment {
+    FrameLayout main_frame;
+    CalendarFragment calendarFragment;
+    GymMapFragment gymMapFragment;
+    ImageButton calendar;
+    ImageButton gym1;
 
     public ExerciseFragment() {
         // Required empty public constructor
@@ -29,7 +37,32 @@ public class ExerciseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercise, container, false);
+        View RootView =  inflater.inflate(R.layout.fragment_exercise, container, false);
+        main_frame = (FrameLayout)RootView.findViewById(R.id.main_frame);
+        calendarFragment = new CalendarFragment();
+        gymMapFragment = new GymMapFragment();
+        calendar = (ImageButton)RootView.findViewById(R.id.calendar);
+        calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setFragment(calendarFragment);
+            }
+        });
+        gym1 = (ImageButton)RootView.findViewById(R.id.gym_location);
+
+        gym1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setFragment(gymMapFragment);
+            }
+        });
+        return RootView;
+    }
+
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame,fragment);
+        fragmentTransaction.commit();
     }
 
 }
